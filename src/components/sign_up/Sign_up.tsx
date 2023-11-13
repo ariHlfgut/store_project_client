8abd27ef1c99448f1a25e931ea4d41193f52338b
Isaschar Madar <pryysy88@gmail.com>
2023-11-13 12:09:10 +0200

    sign up basic with react hook form

--- src/components/sign_up/Sign_up.tsx
+++ src/components/sign_up/Sign_up.tsx
@@ -1,4 +1,56 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+type Inputs = {
+  fullName: string;
+  email: string;
+  city: string;
+  street: string;
+  departmentNumber: number;
+  password: string;
+  passwordConfirmation: string;
+};
+
 export default function Sign_up() {
-  return <h1>sign_up</h1>;
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("fullName", {
+          required: true,
+          maxLength: 20,
+          minLength: 2,
+        })}
+      />
+      <input
+        {...register("email", { required: true, maxLength: 20, minLength: 2 })}
+      />
+      <input
+        {...register("city", { required: true, maxLength: 20, minLength: 2 })}
+      />
+      <input
+        {...register("street", { required: true, maxLength: 20, minLength: 2 })}
+      />
+      <input
+        {...register("departmentNumber", {
+          required: true,
+        })}
+      />
+      <input
+        {...register("password", {
+          required: true,
+          maxLength: 20,
+          minLength: 2,
+        })}
+      />
+      <input
+        {...register("passwordConfirmation", {
+          required: true,
+          maxLength: 20,
+          minLength: 2,
+        })}
+      />
+      <input type="submit" />
+    </form>
+  );
 }
