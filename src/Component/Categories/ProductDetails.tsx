bb077412772df439c3bf859935ab77e3de006777
zevi bem <zevibem@gmail.com>
2023-11-13 09:12:29 +0200

    2 component commit 1 categories component 2 prodact details component

---
+++ src/Component/Categories/ProductDetails.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+interface ProductProps {
+    id: string;
+    name: string;
+    title: string;
+    description: string;
+    price: number;
+    img_url: string;
+    units_in_stock: number;
+    rate: number;
+    count_clicks: number;
+    color: string;
+    model: string;
+}
+
+const ProductDetails: React.FC<ProductProps> = (props: ProductProps) => {
+    return (
+        <div  className='product_details_container'>
+            <div>{props.name}</div>
+            <div>{props.img_url}</div>
+            <div>{props.title}</div>
+            <div>{props.price}</div>
+            <div>{props.description}</div>
+            <div>{props.model}</div>
+            <div>{props.color}</div>
+            <div>{props.units_in_stock}</div>
+            <div>{props.rate}</div>
+        </div>  
+    )
+}
+export default ProductDetails
